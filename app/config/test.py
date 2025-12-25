582eb49e78c0f8ca1bc03b0813aab83c910fd0dd
josh1506 <joshuamichael_jabor@yahoo.com>
2025-12-25 18:28:50 +0800

    Add basic add and subtract test

---
+++ app/config/test.py
@@ -0,0 +1,19 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+
+from config.calc import add, subtract
+
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+        res = add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        res = subtract(10, 15)
+        self.assertEqual(res, 5)
