89a6141f4b37b3a44758bf3a545c0efee7a8980f
josh1506 <joshuamichael_jabor@yahoo.com>
2026-01-30 15:00:37 +0800

    Improve API viewset to allow retrieving recipe details

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -13,7 +13,7 @@ from recipe import serializers
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs."""
 
-    serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -25,3 +25,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
+
+    def get_serializer_class(self):
+        """Return the serializer class for request."""
+        if self.action == "list":
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
