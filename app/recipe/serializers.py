0470b1e0077100a36e29bd914d94ae3069607ff9
josh1506 <joshuamichael_jabor@yahoo.com>
2026-02-18 18:57:41 +0800

    Add create Tag in API

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -7,13 +7,36 @@ from rest_framework import serializers
 from core.models import Recipe, Tag
 
 
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tag objects."""
+
+    class Meta:
+        model = Tag
+        fields = ["id", "name"]
+        read_only_fields = ["id"]
+
+
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipe objects."""
 
+    tags = TagSerializer(many=True, required=False)
+
     class Meta:
         model = Recipe
-        fields = ["id", "title", "time_minutes", "price", "link"]
+        fields = ["id", "title", "time_minutes", "price", "link", "tags"]
         read_only_fields = ["id"]
+
+    def create(self, validated_data):
+        """Create a recipe."""
+        tags_data = validated_data.pop("tags", [])
+        recipe = Recipe.objects.create(**validated_data)
+        auth_user = self.context["request"].user
+        for tag_data in tags_data:
+            tag, created = Tag.objects.get_or_create(
+                user=auth_user, **tag_data
+            )
+            recipe.tags.add(tag)
+        return recipe
 
 
 class RecipeDetailSerializer(RecipeSerializer):
